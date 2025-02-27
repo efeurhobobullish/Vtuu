b6880fcdecc98ef44e5dfe7d5933a967fd63b675
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-27 04:47:39 +0100

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const ServiceSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, enum: ["data", "airtime", "bills", "crypto"], required: true },
+  type: { type: String, enum: ["airtime", "data", "utility"], required: true },
   price: { type: Number, required: true },
-  description: { type: String },
-  active: { type: Boolean, default: true },
+  provider: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Service", ServiceSchema);

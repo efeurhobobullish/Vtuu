d0b82722d5cf48138297c6a49bcaee3f95761907
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-27 05:41:51 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,13 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
+    phone: { type: String, unique: true },
     password: String,
-    balance: { type: Number, default: 0 },
-    verificationCode: Number,
-    verified: { type: Boolean, default: false }
+    isVerified: { type: Boolean, default: false },
+    otp: { type: String, required: false },
+    otpExpires: { type: Date, required: false }
 });
 
-// Hash password before saving
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);

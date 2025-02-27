b5b1122142d3ee4142008785d3be5d97b631ea27
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-27 05:37:04 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,15 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
-exports.authenticateAdmin = (req, res, next) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ error: "Access denied!" });
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ error: 'Access denied' });
 
     try {
-        const verified = jwt.verify(token, "SECRET_KEY");
-        req.admin = verified;
+        const decoded = jwt.verify(token, config.JWT_SECRET);
+        req.user = decoded;
         next();
     } catch (err) {
-        res.status(400).json({ error: "Invalid token!" });
+        res.status(400).json({ error: 'Invalid token' });
     }
 };

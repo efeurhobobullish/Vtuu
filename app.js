bae7eaf16c8ca98e6d76eb118ffb492cae36ae31
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-03-18 00:56:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -135,34 +135,34 @@ app.get("/api/user/balance", async (req, res) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve HTML pages
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/user/docs.html")));
-app.get("/signup", (req, res) => res.sendFile(path.join(__dirname, "../public/user/signup.html")));
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "../public/user/sign-in.html")));
-app.get("/airtime", (req, res) => res.sendFile(path.join(__dirname, "../public/user/airtime.html")));
-app.get("/airtime2cash", (req, res) => res.sendFile(path.join(__dirname, "../public/user/airtime2cash.html")));
-app.get("/cable", (req, res) => res.sendFile(path.join(__dirname, "../public/user/cable.html")));
-app.get("/changePin", (req, res) => res.sendFile(path.join(__dirname, "../public/user/change-pin.html")));
-app.get("/crypto", (req, res) => res.sendFile(path.join(__dirname, "../public/user/crypto.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/user/docs.html")));
+app.get("/signup", (req, res) => res.sendFile(path.join(__dirname, "./public/user/signup.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "./public/user/sign-in.html")));
+app.get("/airtime", (req, res) => res.sendFile(path.join(__dirname, "./public/user/airtime.html")));
+app.get("/airtime2cash", (req, res) => res.sendFile(path.join(__dirname, "./public/user/airtime2cash.html")));
+app.get("/cable", (req, res) => res.sendFile(path.join(__dirname, "./public/user/cable.html")));
+app.get("/changePin", (req, res) => res.sendFile(path.join(__dirname, "./public/user/change-pin.html")));
+app.get("/crypto", (req, res) => res.sendFile(path.join(__dirname, "./public/user/crypto.html")));
 app.get("/dashboard", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/user/dashboard.html"));
 });
-app.get("/data", (req, res) => res.sendFile(path.join(__dirname, "../public/user/data.html")));
-app.get("/docs", (req, res) => res.sendFile(path.join(__dirname, "../public/user/docs.html")));
-app.get("/electricity", (req, res) => res.sendFile(path.join(__dirname, "../public/user/electricity.html")));
-app.get("/forgotPass", (req, res) => res.sendFile(path.join(__dirname, "../public/user/forgot-password.html")));
-app.get("/fundWallet", (req, res) => res.sendFile(path.join(__dirname, "../public/user/fund-wallet.html")));
-app.get("/giftcards", (req, res) => res.sendFile(path.join(__dirname, "../public/user/giftcards.html")));
-app.get("/gotv", (req, res) => res.sendFile(path.join(__dirname, "../public/user/gotv.html")));
-app.get("/investment", (req, res) => res.sendFile(path.join(__dirname, "../public/user/investment.html")));
-app.get("/referrals", (req, res) => res.sendFile(path.join(__dirname, "../public/user/referrals.html")));
-app.get("/services", (req, res) => res.sendFile(path.join(__dirname, "../public/user/services.html")));
-app.get("/settings", (req, res) => res.sendFile(path.join(__dirname, "../public/user/settings.html")));
-app.get("/support", (req, res) => res.sendFile(path.join(__dirname, "../public/user/support.html")));
-app.get("/transactionDetails", (req, res) => res.sendFile(path.join(__dirname, "../public/user/transaction-details.html")));
-app.get("/transaction", (req, res) => res.sendFile(path.join(__dirname, "../public/user/transaction.html")));
-app.get("/updatePass", (req, res) => res.sendFile(path.join(__dirname, "../public/user/update-password.html")));
-app.get("/verify-email", (req, res) => res.sendFile(path.join(__dirname, "../public/user/verify-email.html")));
-app.get("/withdraw", (req, res) => res.sendFile(path.join(__dirname, "../public/user/withdraw.html")));
+app.get("/data", (req, res) => res.sendFile(path.join(__dirname, "./public/user/data.html")));
+app.get("/docs", (req, res) => res.sendFile(path.join(__dirname, "./public/user/docs.html")));
+app.get("/electricity", (req, res) => res.sendFile(path.join(__dirname, "./public/user/electricity.html")));
+app.get("/forgotPass", (req, res) => res.sendFile(path.join(__dirname, "./public/user/forgot-password.html")));
+app.get("/fundWallet", (req, res) => res.sendFile(path.join(__dirname, "./public/user/fund-wallet.html")));
+app.get("/giftcards", (req, res) => res.sendFile(path.join(__dirname, "./public/user/giftcards.html")));
+app.get("/gotv", (req, res) => res.sendFile(path.join(__dirname, "./public/user/gotv.html")));
+app.get("/investment", (req, res) => res.sendFile(path.join(__dirname, "./public/user/investment.html")));
+app.get("/referrals", (req, res) => res.sendFile(path.join(__dirname, "./public/user/referrals.html")));
+app.get("/services", (req, res) => res.sendFile(path.join(__dirname, "./public/user/services.html")));
+app.get("/settings", (req, res) => res.sendFile(path.join(__dirname, "./public/user/settings.html")));
+app.get("/support", (req, res) => res.sendFile(path.join(__dirname, "./public/user/support.html")));
+app.get("/transactionDetails", (req, res) => res.sendFile(path.join(__dirname, "./public/user/transaction-details.html")));
+app.get("/transaction", (req, res) => res.sendFile(path.join(__dirname, "./public/user/transaction.html")));
+app.get("/updatePass", (req, res) => res.sendFile(path.join(__dirname, "./public/user/update-password.html")));
+app.get("/verify-email", (req, res) => res.sendFile(path.join(__dirname, "./public/user/verify-email.html")));
+app.get("/withdraw", (req, res) => res.sendFile(path.join(__dirname, "./public/user/withdraw.html")));
 
 // Start Servers
 app.listen(PORT, () => {
